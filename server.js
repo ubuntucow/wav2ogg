8014a312edff33e4d41b2ae8a01388f7f9e90c3a
ubuntucow <adamypadan@gmail.com>
2024-05-19 11:23:22 +0100

    first

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
 });
